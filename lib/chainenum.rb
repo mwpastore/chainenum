0df1f5e8f4cbb516f3e80ef1fe349173aee5d677
Mike Pastore <mike@oobak.org>
2018-05-10 22:32:59 -0500

    Add size support (and overwrought implementation of Enumerable#chain)

--- lib/chainenum.rb
+++ lib/chainenum.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class Enumerator
-  def chain
-    self.class.new do |yielder|
+  def chain(size=nil)
+    self.class.new(size) do |yielder|
       each do |elem|
         yield yielder, elem
       end
@@ -9,16 +9,27 @@ class Enumerator
   end
 
   class Lazy
-    def chain(&block)
-      self.class.new(self, &block)
+    def chain(size=nil, &block)
+      self.class.new(self, size, &block)
     end
   end if const_defined?(:Lazy)
 end
 
 module Enumerable
-  def chain
-    each_with_object([]) do |elem, memo|
-      yield memo, elem
+  def chain(size=0)
+    memo, index = Array.new(size), 0
+    # we need an object that responds to both #<< and #yield
+    yielder = Enumerator::Yielder.new do |head, *tail|
+      memo[index], index = (tail.empty? ? head : tail.unshift(head)), index.succ
     end
+
+    each do |elem|
+      yield yielder, elem
+    end
+
+    # trim array if we preallocated more space than necessary
+    memo.slice!(index.succ .. -1) if memo.size > index.succ
+
+    memo
   end
 end
