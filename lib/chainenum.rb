684c3880fc817f6be293ff51a51709808cf0cbc8
Mike Pastore <mike@oobak.org>
2018-05-10 22:45:12 -0500

    Use splat for size arg

--- lib/chainenum.rb
+++ lib/chainenum.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class Enumerator
-  def chain(size=nil)
-    self.class.new(size) do |yielder|
+  def chain(*args)
+    self.class.new(*args) do |yielder|
       each do |elem|
         yield yielder, elem
       end
@@ -9,14 +9,14 @@ class Enumerator
   end
 
   class Lazy
-    def chain(size=nil, &block)
-      self.class.new(self, size, &block)
+    def chain(*args, &block)
+      self.class.new(self, *args, &block)
     end
   end if const_defined?(:Lazy)
 end
 
 module Enumerable
-  def chain(size=nil, &block)
-    each.chain(size, &block).to_a
+  def chain(*args, &block)
+    each.chain(*args, &block).to_a
   end
 end
